83d28abede0faf2c8487550afe14bc6d446e8df1
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-12-18 17:55:14 -0500

    added functions to login

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,6 @@ const users = {};
 router.post('/login', (req, res) => {
   //get email and password from request body
   const { email, password } = req.body;
-  console.log(email, password)
 
   userQueries.getUserByEmail(email).then(user => {
     // check if the email is not already in use
@@ -17,20 +16,18 @@ router.post('/login', (req, res) => {
       //send back a response with 404 code for not found email
       return res.status(404).json({ error: 'Email not found' });
     }
-
     //checking if passwords match
     if (!bcrypt.compareSync(password, user[0].password)) {
       //if not return error
       return res.status(403).json({ error: 'incorrect password' })
     };
     // set cookie to user_id
-    req.session.user_id = user.id;
+    req.session.user_id = user[0].id;
     // TODO: switch urls to different redirect
     //redirecting to urls page
     res.redirect('/urls');
 
   }).catch(error => {
-    console.log(error)
     return res.status(500).json({ error: 'An Error Occurred!'});
   });
 });
@@ -39,17 +36,24 @@ router.post('/login', (req, res) => {
 router.get("/login", (req, res) => {
   // getting userId from the cookie
   const userId = req.session.user_id;
-  //getting user object
-  const user = users[userId];
+  console.log(req.session)
+  console.log("userid", userId)
+  //  getting id object
+  userQueries.getUserById(userId).then(user => {
+    if (user) {
+      // TODO: switch urls to different redirect
+      res.redirect('/urls');
+    }
+    const localsVars = {
+      user,
+    };
 
-  const localsVars = {
-    user,
-  };
-  if (user) {
-    // TODO: switch urls to different redirect
-    res.redirect('/urls');
-  }
-  res.render('login', localsVars)
+    //TODO: make sure signUp matches correct page name
+    res.render("login", localsVars);
+  }).catch(error => {
+    console.log(error)
+    return res.status(500).json({ error: 'error invalid data' });
+  })
 });
 
 module.exports = router;
