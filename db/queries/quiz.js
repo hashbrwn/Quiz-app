2b7e7e7fc0439f316542621d8f3050a194376895
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-12-20 00:23:45 -0500

    created create quiz function and endpoint

--- db/queries/quiz.js
+++ db/queries/quiz.js
@@ -8,6 +8,16 @@ const getQuizzesByUser = (id) => {
     });
 }
 
+//Helper function
+const createQuiz = (quizname, private, user_id) => {
+  const query = {
+    text: 'INSERT INTO quizzes (quizname, private, user_id) VALUES ($1, $2, $3) RETURNING *',
+    values: [quizname, private, user_id],
+  }
+  return db.query(query)
+    .then(data => {
+      return data.rows;
+    });
+}
 
-
-module.exports = { getQuizzesByUser };
+module.exports = { getQuizzesByUser, createQuiz };
